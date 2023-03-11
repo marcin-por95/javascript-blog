c65bf83b7df9cfcd0dea378de46de82f0f6a1c76
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-03-11 11:02:30 +0100

    add class 'active' to the correct article */

--- javascript-blog/js/script.js
+++ javascript-blog/js/script.js
@@ -1,31 +1,30 @@
 
-const titleClickHandler = function (event) {
+const titleClickHandler = function(event){
     console.log('Link was clicked!');
-    console.log(event);
-
     /* [DONE] remove class 'active' from all article links  */
-
     const activeLinks = document.querySelectorAll('.titles a.active');
-    for (let activeLink of activeLinks) {
+    for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
     /* [DONE] add class 'active' to the clicked link */
-
+    event.preventDefault();
     const clickedElement = this;
-    console.log('clickedElement:', clickedElement);
-    console.log('clickedElement (with plus): ' + clickedElement);
+    //console.log('clickedElement:', clickedElement);
+    //console.log('clickedElement (with plus): ' + clickedElement);
     clickedElement.classList.add('active');
     /* [DONE] remove class 'active' from all articles */
-
     const activeArticles = document.querySelectorAll('article.active');
-    for (let activeArticle of activeArticles) {
+    for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
-    /*  get 'href' attribute from the clicked link */
-
-    /*  find the correct article using the selector (value of 'href' attribute) */
-
-    /*  add class 'active' to the correct article */
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute("href");
+    console.log(articleSelector);
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+    /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
